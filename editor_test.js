b9890e21d4716e23c09d0c7d24300b4139baa2f9
Zichen Wang <145771726+wasd243@users.noreply.github.com>
2026-03-24 20:29:15 +0000

    table fix-1

--- editor_test.js
+++ editor_test.js
@@ -180,7 +180,6 @@ const wikidotParser = parser.configure({
             "AttrName":            customTags.components,
             "Equals":              customTags.equal,
             "AttrValue":           customTags.Highlight,
-            "Text":                customTags.original_text,
         }),
         foldNodeProp.add({
             "DivBlock": foldInside,
